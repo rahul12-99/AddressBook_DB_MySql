f96bd5653b197b04c3d43e0f94ce52fbac1bf9a8
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-06-07 16:36:16 +0530

    Added : Code for create AddressBook database

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,1 +1,28 @@
 ********* Welcome to addressBook using Mysql *********
+
+mysql> create database Address_Book_Service;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++----------------------+
+| Database             |
++----------------------+
+| address_book_service |
+| addressbook_db       |
+| addressbookdb        |
+| employee             |
+| employee_db          |
+| employeepayroll_db   |
+| information_schema   |
+| mysql                |
+| payroll_service      |
+| payroll_service_jdbc |
+| performance_schema   |
+| rahul                |
+| studentdb            |
+| sys                  |
+| world                |
++----------------------+
+15 rows in set (0.01 sec)
+
+mysql>
